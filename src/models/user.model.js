8ec6a998f4bb5dc6e266aea6dece5471990fa033
vishalBelwal <vishalbelwal52@gmail.com>
2024-10-06 16:55:22 +0530

    user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -29,7 +29,7 @@ const userSchema = new Schema(
             type: String,
             required: true,
         },
-        avatar: {
+        coverImage: {
             type: String,
         },
         watchhistory: [
@@ -53,7 +53,7 @@ userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
 
     //ye karne se baar baar password naya nahi banega
-    this.password = bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
